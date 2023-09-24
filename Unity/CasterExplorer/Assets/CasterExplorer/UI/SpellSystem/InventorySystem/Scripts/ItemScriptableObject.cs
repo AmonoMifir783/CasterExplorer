9f26aaa0916c865b6b7c81a7620fbe5983dc4619
AmonoMifir783 <roman563412@gmail.com>
2023-09-24 12:26:48 +0300

    Amono_InventorySystem001

---
+++ Unity/CasterExplorer/Assets/CasterExplorer/UI/SpellSystem/InventorySystem/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScriptableObject : ScriptableObject
+{
+    public string ItemName;
+    
+    public int Temperature;
+    public int Force;
+    public int Amperage;
+    public int Gravity;
+    public int light;
+}
---
+++ Unity/CasterExplorer/Assets/CasterExplorer/UI/SpellSystem/InventorySystem/Scripts/SpellItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Spell Item", menuName = "Inventory/Items/New Spell Item")]
+public class SpellItem : ItemScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
