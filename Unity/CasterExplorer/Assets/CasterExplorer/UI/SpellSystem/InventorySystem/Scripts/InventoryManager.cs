97041faa9f49f14600c90b6b2f0db48b8c3a2bf8
AmonoMifir783 <roman563412@gmail.com>
2023-10-21 16:45:19 +0300

    Amono_PreparationSaveInventorySlots

--- Unity/CasterExplorer/Assets/CasterExplorer/UI/SpellSystem/InventorySystem/Scripts/InventoryManager.cs
+++ Unity/CasterExplorer/Assets/CasterExplorer/UI/SpellSystem/InventorySystem/Scripts/InventoryManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+
+    public GameObject FastSlot1;
+    public GameObject FastSlot2;
+    public GameObject FastSlot3;
+    public GameObject FastSlot4;
+
+
+    //FastSlot1.GetComponent<InventorySlots>().Item;
+
+
     public Transform InventoryPanel;
     public List<InventorySlots> Slots = new List<InventorySlots>();
 
