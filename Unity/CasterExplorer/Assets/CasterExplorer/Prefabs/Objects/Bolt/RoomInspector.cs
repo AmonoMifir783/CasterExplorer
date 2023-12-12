d3ffccf9294ed334393e17c7482b3e932070be94
HonestJoy67 <albert.mnatsakanyan@internet.ru>
2023-12-13 02:10:24 +0300

    Albert_Puzzle4Fix

--- Unity/CasterExplorer/Assets/CasterExplorer/Prefabs/Objects/Bolt/RoomInspector.cs
+++ Unity/CasterExplorer/Assets/CasterExplorer/Prefabs/Objects/Bolt/RoomInspector.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class RoomInspector : MonoBehaviour
 {
    public Transform target; // Ссылка на объект, который будет перемещаться
-
+    public GameObject lamp;
     public float speed = 0.2f;
     public float targetHeight = 3f;
     private Vector3 initialPosition;
     private float startTime;
-    public bool BatteryEnergy = false;
-    public bool Key = false;    
+    private Material material;
 
     void Start() 
     { 
         initialPosition = target.position;
         startTime = Time.time; // Исправлено использование времени 
         // GetComponent - поиска компонента 
+        Renderer objectRenderer = lamp.GetComponent<Renderer>();
+        material = objectRenderer.material;
     } 
 
     public void OpenTheDoor()
@@ -25,6 +26,9 @@ public class RoomInspector : MonoBehaviour
         int a = transform.GetChild(5).GetComponent<AI_Fire>().currentTemp;
         if (a > transform.GetChild(5).GetComponent<AI_Fire>().minTemp && a < transform.GetChild(5).GetComponent<AI_Fire>().maxTemp)
             {
+                Color baseColor = material.GetColor("_BaseColor");
+                baseColor.g = 170f / 255f; // Присваиваем значение 200 в синий канал (диапазон от 0 до 1)
+                material.SetColor("_BaseColor", baseColor);
                 StartCoroutine(MoveTarget());
             } 
     }
